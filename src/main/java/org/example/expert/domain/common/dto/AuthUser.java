dc85114b29aee3db30ce23179da965a0a41c298d
여준서 <duwnstj12@naver.com>
2024-10-07 22:41:59 +0900

    [update] : Level 2-9 구현 완료

--- src/main/java/org/example/expert/domain/common/dto/AuthUser.java
+++ src/main/java/org/example/expert/domain/common/dto/AuthUser.java
@@ -2,19 +2,28 @@ package org.example.expert.domain.common.dto;
 
 import lombok.Getter;
 import org.example.expert.domain.user.enums.UserRole;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
 
 @Getter
 public class AuthUser {
 
     private final Long id;
     private final String email;
+    private final Collection<? extends GrantedAuthority> authorities;
     private final UserRole userRole;
     private final String nickName;
 
-    public AuthUser(Long id, String email, UserRole userRole, String nickName) {
+    public AuthUser(Long id, String email, UserRole role, String nickName) {
         this.id = id;
         this.email = email;
-        this.userRole = userRole;
+        this.authorities = List.of(new SimpleGrantedAuthority(role.name()));
+        this.userRole = role;
         this.nickName = nickName;
     }
+
+
 }
